400508a22854cefe086515055c9d2c670bc84a2a
Cindy3184 <cindykrampah@yahoo.com>
2021-03-02 15:27:54 -0500

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://quotes15.p.rapidapi.com/quotes/random/?language_code=en",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "70b945a681msh8d168d5c4aa23c1p1bcb28jsn7e989a896147",
-		"x-rapidapi-host": "quotes15.p.rapidapi.com"
-	}
-};
+//constants
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+const $main = $('#main')
+const $author = $('#author')
+
+
+//event listener
+
+$('#btn').click(function(){
+
+	$.ajax("https://quotes15.p.rapidapi.com/quotes/random/", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "70b945a681msh8d168d5c4aa23c1p1bcb28jsn7e989a896147",
+			"x-rapidapi-host": "quotes15.p.rapidapi.com"
+		}
+})
+.then(function(data) {
+	console.log(data)
+	console.log(data.content);
+	$main.text(data.content);
+    $author.text(data.originator.name);
+
+})
+.catch(err => {
+	console.error(err);
+});
+
 });
